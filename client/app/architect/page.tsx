f4a3257bbdff19e0559dc38ad626bcc5ff7039d3
aravindgopan <aravindgopan.dev@gmail.com>
2025-12-22 17:03:32 +0000

    add auth and route protection

---
+++ client/app/architect/page.tsx
@@ -0,0 +1,8 @@
+export default function Architect() {
+  return (
+    <div>
+      <h1>Architect</h1>
+    </div>
+  );
+}
+
---
+++ client/app/designer/page.tsx
@@ -0,0 +1,8 @@
+export default function Designer() {
+  return (
+    <div>
+      <h1>Designer</h1>
+    </div>
+  );
+}
+
